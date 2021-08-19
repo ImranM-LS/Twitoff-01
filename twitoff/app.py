7a11eb16919b4f502c63444829ae18ce9386a829
ImranM-LS <86392870+ImranM-LS@users.noreply.github.com>
2021-08-19 09:47:16 -0400

    Update app.py to fix postgres uri on heroku

--- twitoff/app.py
+++ twitoff/app.py
@@ -5,7 +5,11 @@ from .models import db, User, Tweet
 def create_app():
     app = Flask(__name__)
 
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+    uri = os.getenv("DATABASE_URL")
+    if uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = uri
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     db.init_app(app)
