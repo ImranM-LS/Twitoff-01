3cb497f4fdd55571ce5ad13504d6f046a4143bfc
ImranM-LS <86392870+ImranM-LS@users.noreply.github.com>
2021-08-19 09:31:19 -0400

    Updated reqs.txt, app.py, revomed twitter.py

--- twitoff/app.py
+++ twitoff/app.py
@@ -1,12 +1,11 @@
 import os
 from flask import Flask, render_template, request, redirect
 from .models import db, User, Tweet
-from .twitter import add_or_update_user
 
 def create_app():
     app = Flask(__name__)
 
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URI")
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     db.init_app(app)
